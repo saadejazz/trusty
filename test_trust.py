7e46093ed42ae34144bb14c7d16aa0582248185c
saadejazz <60652466+saadejazz@users.noreply.github.com>
2023-06-09 17:38:32 +0900

    Initial commit

---
+++ test_trust.py
@@ -0,0 +1,25 @@
+from trusty import init_predictor
+from os import listdir
+from os.path import isfile, join
+
+if __name__ == "__main__":
+
+    fpath = "examples/eyecontact_test/"
+    allfiles = [f for f in listdir(fpath) if isfile(join(fpath, f))]
+    print(len(allfiles), "pictures found.")
+
+    trust_estimator = init_predictor()
+    # files = [
+    #     "rgb/image_17_.png",
+    #     "rgb/image_18_.png",
+    #     "rgb/image_19_.png",
+    #     "rgb/image_20_.png",
+    #     "rgb/image_21_.png",
+    #     "rgb/image_22_.png",
+    #     "rgb/image_23_.png",
+    #     "rgb/image_24_.png",
+    #     "rgb/image_25_.png",   
+    #     ]
+    files = [fpath + f for f in allfiles]
+
+    trust_estimator.predict(files)
